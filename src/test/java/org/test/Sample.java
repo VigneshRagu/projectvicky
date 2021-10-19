9ea50e01fab9cba90165c585d92e56656e71b1e6
VigneshRagu <Vigneshraghu972@gmail.com>
2021-10-19 21:49:05 +0530

    Project Structure

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,22 @@
+package org.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Sample {
+	@BeforeClass
+	private void beforeclass() {
+		}
+	@AfterClass
+	private void afterclass() {
+		
+	}
+	@Test
+	private void test() {
+		
+
+	}
+	
+
+}
